2e311eddbe1bfae8a2829096117c3789f6934cf0
Adam Björk <adamhbjork@gmail.com>
2019-09-20 10:54:15 +0200

    Felhantering behöver läggas till

--- Array2/Program.cs
+++ Array2/Program.cs
@@ -12,16 +12,47 @@ namespace Array2
 
             for (int i = 0; i < namnVektor.Length; i++)
             {
-                Console.Write("Namn " + (i + 1) + ": ");
+                Console.Write("Namn " + i + ": ");
                 string namn = Console.ReadLine();
                 namnVektor[i] = namn;
             }
 
             Console.Write("Namnen du matat in är: ");
-            foreach(string i in namnVektor)
+            foreach (string i in namnVektor)
             {
                 Console.Write("{0} ", i);
             }
+
+            Console.ReadLine();
+            Console.WriteLine("\nDu kan nu byta ut ett namn på den indexplats du väljer!");
+            Console.WriteLine("(Skriv Q för att avsluta!)");
+            Console.Write("Välj mellan 0-4: ");
+            string svar = Console.ReadLine();
+            svar = svar.ToUpper();
+
+            if (svar != "Q")
+            {
+                int indexPlats = Convert.ToInt32(svar);
+                Console.Write("Vilket namn ska in på plats " + (indexPlats) + "? ");
+                string nyttNamn = Console.ReadLine();
+                namnVektor[indexPlats] = nyttNamn;
+
+                int index = 0;
+                Console.WriteLine("Namnen du matat in är: ");
+                foreach (string namn in namnVektor)
+                {
+                    Console.WriteLine(index + ": " + namn + " ");
+                    index++;
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Du vill inte byta ut något namn!");
+            }
+
+            Console.WriteLine("Nu avslutas programmet!");
+            Console.ReadLine();
         }
     }
 }
